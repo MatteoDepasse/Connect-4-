a01e76cecc38609ba992e1ffcb8576bf0dff6f09
Kevin Lau <kev2001224@hotmail.com>
2019-01-07 10:04:25 -0500

    Created Frame and Panel
    Added animation panel for graphics

---
+++ AnimationPanel.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class AnimationPanel extends JPanel{
+
+	// Properties
+	
+	
+	// Methods
+	public void paintComponent(Graphics g){
+	
+	}
+	
+	// Constructor
+	public AnimationPanel(){
+		super();
+	}
+}
+	
+	
--- Connect.java
+++ Connect.java
@@ -8,12 +8,22 @@ import java.awt.event.*;
 
 public class Connect{
 	// Properties
-
+	JFrame theframe;
+	AnimationPanel thepanel;
+	
 	// Methods
 
 	// Constructor
 	public Connect(){
+		theframe = new JFrame("Connect 4");
+		thepanel = new AnimationPanel();
+		thepanel.setLayout(null);
+		thepanel.setPreferredSize(new Dimension(1280,720));
 		
+		theframe.setContentPane(thepanel);
+		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theframe.pack();
+		theframe.setVisible(true);
 	}
 	// Main Method
 public static void main (String[] args){
