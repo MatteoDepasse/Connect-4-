a323f0398f903ec1e41843c1e20aaf7761958ba8
Kevin Lau <kev2001224@hotmail.com>
2019-01-08 09:52:01 -0500

    Created Half the Board with Animation Panel

--- AnimationPanel.java
+++ AnimationPanel.java
@@ -11,7 +11,41 @@ public class AnimationPanel extends JPanel{
 	
 	// Methods
 	public void paintComponent(Graphics g){
-	
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 1280, 720);
+		g.setColor(Color.BLUE);
+		g.fillRect(550, 100, 685, 584);	
+		g.setColor(Color.WHITE);
+		g.fillOval(570, 120, 75, 75);
+		g.fillOval(570, 215, 75, 75);
+		g.fillOval(570, 310, 75, 75);
+		g.fillOval(570, 405, 75, 75);
+		g.fillOval(570, 500, 75, 75);
+		g.fillOval(570, 595, 75, 75);
+		g.fillOval(665, 120, 75, 75);
+		g.fillOval(665, 215, 75, 75);
+		g.fillOval(665, 310, 75, 75);
+		g.fillOval(665, 405, 75, 75);
+		g.fillOval(665, 500, 75, 75);
+		g.fillOval(665, 595, 75, 75);
+		g.fillOval(760, 120, 75, 75);
+		g.fillOval(760, 215, 75, 75);
+		g.fillOval(760, 310, 75, 75);
+		g.fillOval(760, 405, 75, 75);
+		g.fillOval(760, 500, 75, 75);
+		g.fillOval(760, 595, 75, 75);
+		g.fillOval(855, 120, 75, 75);
+		g.fillOval(855, 215, 75, 75);
+		g.fillOval(855, 310, 75, 75);
+		g.fillOval(855, 405, 75, 75);
+		g.fillOval(855, 500, 75, 75);
+		g.fillOval(855, 595, 75, 75);
+		g.fillOval(950, 120, 75, 75);
+		g.fillOval(950, 215, 75, 75);
+		g.fillOval(950, 310, 75, 75);
+		g.fillOval(950, 405, 75, 75);
+		g.fillOval(950, 500, 75, 75);
+		g.fillOval(950, 595, 75, 75);
 	}
 	
 	// Constructor
