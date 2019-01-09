b9fdfbc786f684e8e16475962940daa147f2d1c8
Kevin Lau <kev2001224@hotmail.com>
2019-01-09 10:37:10 -0500

    Added second animation panel

---
+++ AnimationPanel2.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class AnimationPanel2 extends JPanel{
+
+	// Properties
+	
+	// Methods
+	
+	public void paintComponent(Graphics g){
+		
+	}
+	
+	// Constructor
+	public AnimationPanel2(){
+		super();
+	}
+}
+	
