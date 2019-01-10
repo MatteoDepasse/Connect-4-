da3ab3c3fd50865c6547e5b7e3f6a74cd4d44930
Kevin Lau <kev2001224@hotmail.com>
2019-01-10 10:56:29 -0500

    Organized

--- Connect.java
+++ Connect.java
@@ -8,8 +8,8 @@ import java.awt.event.*;
 
 
 public class Connect implements ActionListener, MouseMotionListener{
-	// Properties
 	
+	// Properties
 	JFrame theframe;
 	AnimationPanel mainpanel;
 	AnimationPanel2 helppanel;
@@ -34,29 +34,26 @@ public class Connect implements ActionListener, MouseMotionListener{
 
 	// Methods
 	public void actionPerformed(ActionEvent evt){
-		if(evt.getSource()== butPLAY){
-			theframe.setContentPane(netpanel);
-
-		}
 		if(evt.getSource()== butHOWTO){
 			theframe.setContentPane(helppanel);
-			
 			thelabel.setText("Once you click play the game will ask you if you want to be a client or host"); 
 			thelabel2.setText("Choose client or host,the host has to provides the port number and the ip address while the client enters it"); 
 			thelabel3.setText("While playing the turns alternate between red and yellow team"); 
 			thelabel4.setText("Drag and drop your pieces to drop them down a column");
 			thelabel5.setText("Line 4 pieces of your colour to win the game!!");
 		}
+		if(evt.getSource()== butPLAY){
+			theframe.setContentPane(netpanel);
+		}
 		if(evt.getSource()== butBACK){
 			theframe.setContentPane(mainpanel);
 		}
 		if(evt.getSource()== butEXIT){
 			theframe.dispatchEvent(new WindowEvent(theframe, WindowEvent.WINDOW_CLOSING));
-
 		}
-		if(evt.getSource()==thetimer){
+		if(evt.getSource()== thetimer){
 			mainpanel.repaint();
-			//need to repaint the other panels 
+			netpanel.repaint();
 		}
 	}
 	public void mouseDragged(MouseEvent evt){
@@ -80,15 +77,19 @@ public class Connect implements ActionListener, MouseMotionListener{
 		netpanel.setLayout(null);
 		netpanel.setPreferredSize(new Dimension(1280,720));
 		
+		playpanel = new AnimationPanel4();
+		playpanel.setLayout(null);
+		playpanel.setPreferredSize(new Dimension(1280,720));
+		
 		client = new JRadioButton("client");
 		client.addActionListener(this);
 		client.setSize(100,100);
-		client.setLocation(0,0); 
+		client.setLocation(0,100); 
 		
 		host = new JRadioButton("host");
 		host.addActionListener(this);
 		host.setSize(100,100);
-		host.setLocation(100,0); 
+		host.setLocation(100,100); 
 		
 		butPLAY = new JButton("PLAY");
 		butSCORE = new JButton("HIGH SCORE");
@@ -103,7 +104,8 @@ public class Connect implements ActionListener, MouseMotionListener{
 		butEXIT.addActionListener(this);
 		butBACK.addActionListener(this);
 		thetimer = new Timer(1000/60, this);
-		thetimer.start(); 
+		thetimer.start();
+		 
 		thelabel = new JLabel("");
 		thelabel.setSize(1000,50);
 		thelabel.setLocation(0,0);
@@ -124,8 +126,6 @@ public class Connect implements ActionListener, MouseMotionListener{
 		thelabel5.setSize(1000,50);
 		thelabel5.setLocation(0,200);
 
-
-
 		butPLAY.setSize(100,50); 
 		butPLAY.setLocation(0,0);
 		butSCORE.setSize(100,50); 
@@ -137,7 +137,7 @@ public class Connect implements ActionListener, MouseMotionListener{
 		butEXIT.setSize(100,50); 
 		butEXIT.setLocation(0,200);
 		butBACK.setSize(100,50); 
-		butBACK.setLocation(0,300);
+		butBACK.setLocation(20,650);
 		
 		mainpanel.add(butPLAY);
 		mainpanel.add(butSCORE);
@@ -155,16 +155,14 @@ public class Connect implements ActionListener, MouseMotionListener{
 		
 		netpanel.add(host);
 		netpanel.add(client); 
-		netpanel.add(butBACK); 
-
 		
 		theframe.setContentPane(mainpanel);
 		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		theframe.pack();
-		theframe.setVisible(true);
-		
+		theframe.setVisible(true);	
 		
 	}
+	
 	// Main Method
 public static void main (String[] args){
 	new Connect();
