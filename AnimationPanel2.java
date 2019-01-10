75253d5e7b4b6dc758409c5b99ab87d305a63e3b
Kevin Lau <kev2001224@hotmail.com>
2019-01-10 10:29:22 -0500

    Added pictures to help panel

--- AnimationPanel2.java
+++ AnimationPanel2.java
@@ -7,16 +7,32 @@ import javax.imageio.*;
 public class AnimationPanel2 extends JPanel{
 
 	// Properties
-	
+	BufferedImage imgwinup;
+	BufferedImage imgwinright;
+	BufferedImage imgwindiag;
+	BufferedImage imgwindiag2;
 	
 	// Methods
 	public void paintComponent(Graphics g){
+		
+		g.drawImage(imgwinup, 100, 300, null);
+		g.drawImage(imgwinright, 100, 550, null);
+		g.drawImage(imgwindiag, 100, 800, null);
+		g.drawImage(imgwindiag2, 100, 1050, null);
 		
 	}
 	
 	// Constructor
 	public AnimationPanel2(){
 		super();
+		try{
+			imgwinup = ImageIO.read(new File("winup.png"));
+			imgwinright = ImageIO.read(new File("winright.png"));
+			imgwindiag = ImageIO.read(new File("windiag.png"));
+			imgwindiag2 = ImageIO.read(new File("windiag2.png"));
+		}catch(IOException e){
+			System.out.println("Unable to load");
+		}
 	}
 }
 	
